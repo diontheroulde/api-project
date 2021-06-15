e81d64ceaebd400870f1a85e245712ca25b8f10d
Dion Theroulde <dionjr105@gmail.com>
2021-06-15 09:31:54 -0400

    Saved Image List

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const submitBtn = document.querySelector("#search-form")
 const submitBox = document.querySelector("input#search")
-    
+const imageUl = document.querySelector("ul#image-list")
+
 
 submitBtn.addEventListener("submit", (event) => {
     event.preventDefault()
@@ -13,31 +14,45 @@ submitBtn.addEventListener("submit", (event) => {
 fetch(`https://images-api.nasa.gov/search?q=${searchInput}&media_type=image`)
     .then(res => res.json())
     .then(res => {
-        
-        
-        res.collection.items.forEach(item => {
+        res.collection.items.slice(0,10).forEach(item => {
           if(item.links === undefined) {return}
-            item.links.forEach(link => {
-                const imageUl = document.querySelector("ul#image-list")
+            item.links.slice(0,10).forEach(link => {
                 const imageList = document.createElement('li')
                 const spaceImage = document.createElement('img')
                 spaceImage.src = link.href
                 spaceImage.height = 100
-            imageList.append(spaceImage)
+                const likeBtn = document.createElement("button")
+                likeBtn.innerHTML = "$"
+                likeBtn.addEventListener("click", (event) => {
+                    event.preventDefault()
+                    const likedImageUl = document.querySelector("#savedImage-list")
+                    const likedImageList = document.createElement("li")
+                    const saveLikedImage = document.querySelector("img")
+                    saveLikedImage.src = spaceImage.src
+                    likedImageList.append(saveLikedImage)
+                    likedImageUl.append(likedImageList)
+                })
+               
+            imageList.append(spaceImage, likeBtn)
             imageUl.append(imageList)
             })
-            for (let i = 0; i < 10; i ++){
-         return item}
-            
         })
         
     })
-
  })
+
+        
+
+
+
+    
+
+
+    
 
 })
 
 
 
-
+//♥
 
