8f33f5c5b505fc9661808fc261dff48bd3ac977d
Dion Theroulde <dionjr105@gmail.com>
2021-06-17 14:27:58 -0400

    Review w/ Wade

--- src/index.js
+++ src/index.js
@@ -1,14 +1,56 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const submitBtn = document.querySelector("#search-form")
-  const submitBox = document.querySelector("input#search")
+function displayImage(link) {
+  const spaceListItem = document.createElement("li")
+  const spaceImage = document.createElement("img")
+  const likeBtn = document.createElement("button")
   const imageUl = document.querySelector("ul#image-list")
 
-  
+  spaceImage.src = link.href
+  spaceImage.height = 150
+  likeBtn.innerHTML = "♥"
+  spaceListItem.append(likeBtn)
+  spaceListItem.append(spaceImage)
+  imageUl.append(spaceListItem)
+  likeBtn.addEventListener("click", (event) => {
+    event.preventDefault()
+    const likedImageUl = document.querySelector("#savedImage-list")
+    const likeImageListItem = document.createElement("li")
+    const dislikeBtn = document.createElement("button")
+
+    dislikeBtn.innerHTML = "X"
+    dislikeBtn.addEventListener("click", (event) => {
+      event.preventDefault()
+      likeImageListItem.remove()
+    })
+    
+    const saveLikedImage = document.createElement("img")
+
+    saveLikedImage.src = link.href
+    saveLikedImage.height = 90
+    console.log(saveLikedImage)
+    likeImageListItem.insertAdjacentElement("beforeend", saveLikedImage)
+    likeImageListItem.insertAdjacentElement("beforeend", dislikeBtn)
+    likedImageUl.insertAdjacentElement("beforeend", likeImageListItem)
+  })
+
+}
+
+
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const submitBtn = document.querySelector("#search-form")
 
   submitBtn.addEventListener("submit", (event) => {
     event.preventDefault()
-    const searchInput = submitBox.value
+    const searchInput = document.querySelector("input#search").value
     searchInput
+    const imageUl = document.querySelector("ul#image-list")
 
     fetch(`https://images-api.nasa.gov/search?q=${searchInput}&media_type=image`)
       .then(res => res.json())
@@ -17,33 +59,7 @@ document.addEventListener("DOMContentLoaded", () => {
         res.collection.items.slice(0, 20).forEach(item => {
           if (item.links === undefined) { return }
           item.links.slice(0, 20).forEach(link => {
-            const spaceListItem = document.createElement("li")
-            const spaceImage = document.createElement("img")
-            const likeBtn = document.createElement("button")
-            spaceImage.src = link.href
-            spaceImage.height = 150
-            likeBtn.innerHTML = "♥"
-            spaceListItem.append(likeBtn)
-            spaceListItem.append(spaceImage)
-            imageUl.append(spaceListItem)
-            likeBtn.addEventListener("click", (event) => {
-              event.preventDefault()
-              const likedImageUl = document.querySelector("#savedImage-list")
-              const likeImageListItem = document.createElement("li")
-              const dislikeBtn = document.createElement("button")
-              dislikeBtn.innerHTML = "X"
-              dislikeBtn.addEventListener("click", (event) => {
-                event.preventDefault()
-                likeImageListItem.remove()
-              })
-              const saveLikedImage = document.createElement("img")
-              saveLikedImage.src = link.href
-              saveLikedImage.height = 90
-              console.log(saveLikedImage)
-              likeImageListItem.insertAdjacentElement("beforeend", saveLikedImage)
-              likeImageListItem.insertAdjacentElement("beforeend", dislikeBtn)
-              likedImageUl.insertAdjacentElement("beforeend", likeImageListItem)
-            })
+            displayImage(link)
           })
         })
       })
@@ -53,8 +69,11 @@ document.addEventListener("DOMContentLoaded", () => {
     event.preventDefault()
     const commentTextArea = document.querySelector('textarea')
     const newComment = document.createElement("li")
+
     newComment.innerHTML = commentTextArea.value
     document.querySelector('.comments').append(newComment)
+    window.scrollTo(0,document.body.scrollHeight);
+    
     
     
     
